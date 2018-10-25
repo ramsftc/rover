2200a8130248dcf19b7014c7a0c31de2219bd656
FTCdev <ramsftc@gmail.com>
2018-10-24 22:05:28 -0500

    Add gamepad 2 and arm
    we added functionality to the arm with gamepad 2

--- PushbotTeleopPOV_Linear.java
+++ PushbotTeleopPOV_Linear.java
@@ -86,6 +86,13 @@ public class PushbotTeleopPOV_Linear extends LinearOpMode {
             // This way it's also easy to just drive straight, or just turn.
             drive = -gamepad1.left_stick_y;
             turn  =  gamepad1.right_stick_x;
+            
+            if (gamepad2.y)
+                robot.mainArm.setPower(robot.MAIN_ARM_UP);
+            else if (gamepad2.a)
+                robot.mainArm.setPower(robot.MAIN_ARM_DOWN);
+            else
+                robot.mainArm.setPower(0.0);
 
             // Combine drive and turn for blended motion.
             left  = drive + turn;
@@ -103,17 +110,13 @@ public class PushbotTeleopPOV_Linear extends LinearOpMode {
             robot.leftDrive.setPower(left);
             robot.rightDrive.setPower(right);
 
-			/*
+            /*
             // Use gamepad left & right Bumpers to open and close the claw
             if (gamepad1.right_bumper)
                 clawOffset += CLAW_SPEED;
             else if (gamepad1.left_bumper)
                 clawOffset -= CLAW_SPEED;
 
-            // Move both servos to new position.  Assume servos are mirror image of each other.
-            clawOffset = Range.clip(clawOffset, -0.5, 0.5);
-            robot.leftClaw.setPosition(robot.MID_SERVO + clawOffset);
-            robot.rightClaw.setPosition(robot.MID_SERVO - clawOffset);
 
             // Use gamepad buttons to move arm up (Y) and down (A)
             if (gamepad1.y)
@@ -123,7 +126,7 @@ public class PushbotTeleopPOV_Linear extends LinearOpMode {
             else
                 robot.leftArm.setPower(0.0);
 
-			*/
+            */
             // Send telemetry message to signify robot running;
             telemetry.addData("claw",  "Offset = %.2f", clawOffset);
             telemetry.addData("left",  "%.2f", left);
